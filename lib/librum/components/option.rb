5a49f0179897df73d17921aa6466b54eef5cb403
Rob Smith <sleepingkingstudios@gmail.com>
2025-03-11 23:39:18 -0400

    Implement Option#required?, #validate.

--- lib/librum/components/option.rb
+++ lib/librum/components/option.rb
@@ -4,18 +4,29 @@ require 'librum/components'
 
 module Librum::Components
   # Value class representing an optional parameter for a component.
-  Option = Data.define(:name, :boolean, :default) do
+  Option = Data.define(:name, :boolean, :default, :required, :validate) do
     # @param name [String, Symbol] the name of the option.
     # @param boolean [true, false] if true, indicates that the option is a
     #   boolean option and should have only true or false values.
     # @param default [Proc, Object] the default value for the object.
-    def initialize(name:, boolean: false, default: nil)
+    # @param required [true, false] if true, indicates that the option is
+    #   required for the component.
+    # @param validate [Symbol, Class, Proc, nil] the validation for the option,
+    #   if any.
+    def initialize(
+      name:,
+      boolean:  false,
+      default:  nil,
+      required: false,
+      validate: nil
+    )
       name = name.to_s
       name = name.sub(/\?\z/, '') if boolean
 
+      boolean = false if boolean.nil?
       default = false if boolean && default.nil?
 
-      super(boolean: boolean ? true : false, name:, default:)
+      super(boolean:, name:, default:, required:, validate:)
     end
 
     # @return [true, false] if true, indicates that the option is a boolean
@@ -27,6 +38,17 @@ module Librum::Components
     # @return [true, false] if true, the option defines a default value.
     def default?
       !default.nil?
+    end
+
+    # @return [true, false] if true, indicates that the option is required for
+    #   the component.
+    def required?
+      required
+    end
+
+    # @return [true, false] if true, the option defines a validation.
+    def validate?
+      !validate.nil?
     end
   end
 end
