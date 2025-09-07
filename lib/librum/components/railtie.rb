8faa5ade74026a6fe465afe935c0d92ab1b8d409
Rob Smith <sleepingkingstudios@gmail.com>
2025-09-07 15:53:48 -0400

    Handle Zeitwerk reloading in Railtie.

--- lib/librum/components/railtie.rb
+++ lib/librum/components/railtie.rb
@@ -6,9 +6,32 @@ module Librum::Components
   # Initializer for configuring the components in a Rails application.
   class Railtie < Rails::Railtie
     # :nocov:
+    class Reloader
+      def initialize(file_watcher)
+        @file_watcher = file_watcher
+      end
+
+      def updated?
+        @file_watcher.execute_if_updated
+      end
+    end
+
     initializer 'librum-components.append_assets_path', group: :all do |app|
       app.config.assets.paths << Librum::Components.javascript_path
       app.config.assets.paths << Librum::Components.stylesheets_path
+    end
+
+    initializer 'librum-components.enable_reloading' do
+      next unless Rails.application.config.enable_reloading
+
+      tag          = 'Librum-components'
+      loader_path  = Pathname.new(Librum::Components.root_path).glob('**/*')
+      loader       =
+        Zeitwerk::Registry.loaders.each.find { |loader| loader.tag = tag }
+      file_watcher =
+        ActiveSupport::FileUpdateChecker.new(loader_path) { loader.reload }
+
+      Rails.application.reloaders << Reloader.new(file_watcher)
     end
 
     initializer 'librum-components.importmap', before: 'importmap' do |app|
