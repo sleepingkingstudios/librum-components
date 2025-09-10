110564dee4d29dd0fec34f5e53799b1f455b545a
Rob Smith <sleepingkingstudios@gmail.com>
2025-09-10 11:30:31 -0400

    Fix load order dependency for MissingView spec.

--- lib/librum/components/views/missing_view.rb
+++ lib/librum/components/views/missing_view.rb
@@ -1,19 +1,11 @@
 # frozen_string_literal: true
 
+require 'pp'
+
 module Librum::Components::Views
   # View rendered when the expected view is not defined.
   class MissingView < Librum::Components::View
     option :expected_page,   validate: String
     option :view_paths,      validate: { array: String }
-
-    private
-
-    def pretty_inspect(value)
-      return nil if value.nil?
-
-      return value.pretty_inspect if value.respond_to?(:pretty_inspect)
-
-      value.inspect
-    end
   end
 end
