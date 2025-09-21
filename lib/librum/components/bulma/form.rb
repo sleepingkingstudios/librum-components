194d33a9081c477f0d89f533f2c376cd2cb89dd6
Rob Smith <sleepingkingstudios@gmail.com>
2025-09-21 15:10:32 -0400

    Add :columns option to Bulma::Form.

--- lib/librum/components/bulma/form.rb
+++ lib/librum/components/bulma/form.rb
@@ -12,7 +12,38 @@ module Librum::Components::Bulma
     ICONLESS_TYPES = Set.new(%w[checkbox select textarea]).freeze
     private_constant :ICONLESS_TYPES
 
+    option :columns, validate: Integer
+
     private
+
+    def build_buttons(options:)
+      return super unless options.key?(:col_span) || options.key?(:colspan)
+
+      col_span = options.fetch(:col_span, options[:colspan])
+      options  = options.except(:col_span, :colspan).merge(
+        class_name: bulma_class_names("cell is-col-span-#{col_span}")
+      )
+
+      super
+    end
+
+    def build_input(name:, options:, type:)
+      return super unless options.key?(:col_span) || options.key?(:colspan)
+
+      col_span = options.fetch(:col_span, options[:colspan])
+      options  = options.except(:col_span, :colspan).merge(
+        class_name: bulma_class_names("cell is-col-span-#{col_span}")
+      )
+
+      super
+    end
+
+    def form_class_name
+      class_names(
+        super,
+        columns ? bulma_class_names("fixed-grid has-#{columns}-cols") : nil
+      )
+    end
 
     def options_for_errors(errors:, type:, **options)
       super.tap do |hsh|
@@ -22,6 +53,12 @@ module Librum::Components::Bulma
           hsh[:icon_right] = 'exclamation-triangle'
         end
       end
+    end
+
+    def render_content
+      return super unless columns
+
+      content_tag('div', class: bulma_class_names('grid')) { super }
     end
   end
 end
--- spec/librum/components/bulma/form_spec.rb
+++ spec/librum/components/bulma/form_spec.rb
@@ -30,6 +30,180 @@ do
   let(:result)            { Cuprum::Rails::Result.new }
   let(:required_keywords) { { result: } }
 
+  include_deferred 'should define component option', :columns, value: 3
+
+  describe '.new' do
+    define_method :validate_options do
+      described_class.new(**required_keywords, **component_options)
+    end
+
+    include_deferred 'should validate the type of option',
+      :columns,
+      allow_nil: true,
+      expected:  Integer
+  end
+
+  describe '#buttons' do
+    let(:options) { {} }
+    let(:buttons) { component.buttons(**options) }
+
+    context 'when the buttons component is defined' do
+      let(:expected_options) { options }
+
+      prepend_before(:example) do
+        stub_provider(
+          Librum::Components.provider,
+          :components,
+          Spec::Components
+        )
+      end
+
+      example_class 'Spec::Components::Forms::Buttons',
+        Librum::Components::Base \
+      do |klass|
+        klass.allow_extra_options
+
+        klass.option :text
+      end
+
+      context 'when initialized with columns: value' do
+        let(:component_options) { super().merge(columns: 3) }
+
+        describe 'with colspan: value' do
+          let(:options) { super().merge(colspan: 2) }
+          let(:expected_options) do
+            super().except(:colspan).merge(class_name: 'cell is-col-span-2')
+          end
+
+          it { expect(buttons.options).to be == expected_options }
+        end
+      end
+    end
+  end
+
+  describe '#call' do
+    describe 'with action: value and columns: value' do
+      let(:component_options) { super().merge(action: '/rockets', columns: 3) }
+      let(:snapshot) do
+        <<~HTML
+          <form class="fixed-grid has-3-cols" action="/rockets" accept-charset="UTF-8" data-remote="true" method="post">
+            <input name="utf8" type="hidden" value="✓" autocomplete="off">
+
+            <input type="hidden" name="authenticity_token" value="[token]" autocomplete="off">
+
+            <div class="grid"></div>
+          </form>
+        HTML
+      end
+
+      before(:example) do
+        allow(component).to receive_messages( # rubocop:disable RSpec/SubjectStub
+          form_authenticity_token:  '12345',
+          protect_against_forgery?: true
+        )
+      end
+
+      it { expect(rendered).to match_snapshot(snapshot) }
+    end
+
+    describe 'with columns: value' do
+      let(:component_options) { super().merge(columns: 3) }
+      let(:snapshot) do
+        <<~HTML
+          <form class="fixed-grid has-3-cols">
+            <div class="grid"></div>
+          </form>
+        HTML
+      end
+
+      it { expect(rendered).to match_snapshot(snapshot) }
+
+      describe 'with contents' do
+        let(:contents) do
+          component.content_tag('div') { 'Form Inputs' }
+        end
+        let(:component) do
+          super().with_content(contents)
+        end
+        let(:snapshot) do
+          <<~HTML
+            <form class="fixed-grid has-3-cols">
+              <div class="grid">
+                <div>
+                  Form Inputs
+                </div>
+              </div>
+            </form>
+          HTML
+        end
+
+        it { expect(rendered).to match_snapshot(snapshot) }
+      end
+
+      describe 'with fields from a form builder' do
+        let(:block) do
+          lambda do |builder|
+            builder.fields << component.content_tag('h1') { 'Form Heading' }
+
+            builder.input('rocket[name]', colspan: 2)
+
+            builder.checkbox('rocket[refuel]')
+
+            builder.text_area('rocket[description]', colspan: 3)
+          end
+        end
+        let(:component) { super().build(&block) }
+        let(:snapshot) do
+          <<~HTML
+            <form class="fixed-grid has-3-cols">
+              <div class="grid">
+                <h1>
+                  Form Heading
+                </h1>
+
+                <div class="field cell is-col-span-2">
+                  <label class="label">
+                    Name
+                  </label>
+
+                  <div class="control">
+                    <input name="rocket[name]" class="input" type="text">
+                  </div>
+                </div>
+
+                <div class="field">
+                  <div class="control">
+                    <label class="checkbox">
+                      <input autocomplete="off" name="rocket[refuel]" type="hidden" value="0">
+
+                      <input name="rocket[refuel]" type="checkbox" value="1">
+
+                      <span class="ml-1">
+                        Refuel
+                      </span>
+                    </label>
+                  </div>
+                </div>
+
+                <div class="field cell is-col-span-3">
+                  <label class="label">
+                    Description
+                  </label>
+
+                  <div class="control">
+                    <textarea name="rocket[description]" class="textarea"></textarea>
+                  </div>
+                </div>
+              </div>
+            </form>
+          HTML
+        end
+
+        it { expect(rendered).to match_snapshot(snapshot) }
+      end
+    end
+  end
+
   describe '#checkbox' do
     let(:name)    { 'rocket[refuel]' }
     let(:options) { {} }
@@ -61,6 +235,19 @@ do
         end
 
         it { expect(input.options).to be == expected_options }
+      end
+
+      context 'when initialized with columns: value' do
+        let(:component_options) { super().merge(columns: 3) }
+
+        describe 'with colspan: value' do
+          let(:options) { super().merge(colspan: 2) }
+          let(:expected_options) do
+            super().except(:colspan).merge(class_name: 'cell is-col-span-2')
+          end
+
+          it { expect(input.options).to be == expected_options }
+        end
       end
     end
   end
@@ -178,6 +365,19 @@ do
           it { expect(input.options).to be == expected_options }
         end
       end
+
+      context 'when initialized with columns: value' do
+        let(:component_options) { super().merge(columns: 3) }
+
+        describe 'with colspan: value' do
+          let(:options) { super().merge(colspan: 2) }
+          let(:expected_options) do
+            super().except(:colspan).merge(class_name: 'cell is-col-span-2')
+          end
+
+          it { expect(input.options).to be == expected_options }
+        end
+      end
     end
   end
 
@@ -220,6 +420,19 @@ do
 
         it { expect(input.options).to be == expected_options }
       end
+
+      context 'when initialized with columns: value' do
+        let(:component_options) { super().merge(columns: 3) }
+
+        describe 'with colspan: value' do
+          let(:options) { super().merge(colspan: 2) }
+          let(:expected_options) do
+            super().except(:colspan).merge(class_name: 'cell is-col-span-2')
+          end
+
+          it { expect(input.options).to be == expected_options }
+        end
+      end
     end
   end
 
@@ -254,6 +467,19 @@ do
         end
 
         it { expect(input.options).to be == expected_options }
+      end
+
+      context 'when initialized with columns: value' do
+        let(:component_options) { super().merge(columns: 3) }
+
+        describe 'with colspan: value' do
+          let(:options) { super().merge(colspan: 2) }
+          let(:expected_options) do
+            super().except(:colspan).merge(class_name: 'cell is-col-span-2')
+          end
+
+          it { expect(input.options).to be == expected_options }
+        end
       end
     end
   end
