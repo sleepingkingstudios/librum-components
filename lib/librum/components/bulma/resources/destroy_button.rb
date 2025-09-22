f08703a0ef7aaa817644215d485197627aa61c1e
Rob Smith <sleepingkingstudios@gmail.com>
2025-09-22 19:44:51 -0400

    Implement Bulma::Resources::DestroyButton.

---
+++ lib/librum/components/bulma/resources/destroy_button.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Librum::Components::Bulma::Resources
+  # Utility component for rendering the destroy button for a resource.
+  class DestroyButton < Librum::Components::Bulma::Base
+    option :icon,
+      default:  'eraser',
+      validate: true
+    option :text,
+      default:  'Destroy',
+      validate: String
+    option :url,
+      required: true,
+      validate: String
+
+    # @return [ActiveSupport::SafeBuffer] the rendered button.
+    def call
+      render components::Button.new(**button_options)
+    end
+
+    private
+
+    def button_class_name
+      bulma_class_names('is-outlined')
+    end
+
+    def button_options
+      {
+        color:       configuration.danger_color,
+        class_name:  button_class_name,
+        http_method: 'delete',
+        icon:,
+        text:,
+        type:        'form',
+        url:
+      }
+    end
+  end
+end
